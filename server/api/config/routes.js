62ddb3dbe50ff98469427a6533f6f694e3404d62
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2019-12-20 13:56:10 -0800

    add cors to backend

--- server/api/config/routes.js
+++ server/api/config/routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/get-name", (req, res) => {
-  console.log("REACHES HERE@!!!: ", req.params);
+  console.log("REACHES HERE@!!!: ", req);
   res.status(200).json({ name: req.params.name });
 });
 
